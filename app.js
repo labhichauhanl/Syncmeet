96eaf84d1d330b8a7c67f133f28236db761a5cab
Abhyudai Singh <abhichauhan4804@gmail.com>
2026-09-08 15:10:09 +0530

    structuring backend and mongodb

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 import express from "express";
 import { createServer } from "node:http";
-import { Server } from "socket.io";
-import {connectToSocket} from "./backend/src/controllers/socketManager"
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectToSocket from "./backend/src/controllers/socketManager.js";
+import userRoutes from "./backend/src/routes/usersroutes.js";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const io = connectToSocket(server);
+
+connectToSocket(server);
 
 app.use(cors());
-app.use(express.json({limit: "40kb"}));
-app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+app.use("/api/v1/users", userRoutes);
 
-const start = async ()=>{
-    const connectionDb = await mongoose.connect("mongodb+srv://abhichauhan4804:zv2mzEDacSJephQ6@cluster1.fgdszp5.mongodb.net/?appName=Cluster1")
-    console.log("Connected to MongoDb")
-    app.listen(8000, ()=>{
-        console.log("Server is listening on port 3000");
-    })
+const start = async () => {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Connected to MongoDB");
+
+    server.listen(8000, () => {
+        console.log("Server is listening on port 8000");
+    });
 };
+
 start();
-
-
-
-
-
-
-
-
-
